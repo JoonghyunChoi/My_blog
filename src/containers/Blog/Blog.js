3fd00aafa9c9716a6a77136a5f29f35b4fcbe1ae
최중현 <joonghyun@choejunghyeon.local>
2020-10-11 20:18:49 +0900

    Paging and Comment

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,13 @@
 import React, { Component, Suspense } from 'react';
 import './Blog.css';
+import NewPost from './NewPost/NewPost'
 import Posts from './Posts/Posts';
 import QA from './QA/QA';
 import Message from './Message/Message';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import FullPost from '../../containers/Blog/FullPost/FullPost';
 
-const NewPost = React.lazy(() => import('./NewPost/NewPost')); //Async importing
+// const NewPost = React.lazy(() => import('./NewPost/NewPost')); //Async importing
 
 class Blog extends Component {
     render () {
@@ -14,8 +15,8 @@ class Blog extends Component {
             <div className="Blog">
                 <Switch>
                     <Route path='/new-post' exact component={NewPost}/>
-                    <Route path='/qa' component={QA}/>
-                    <Route path='/message' component={Message}/>
+                    <Route path={'/qa'} component={QA}/>
+                    <Route path={'/message:id'} component={Message}/>
                     <Route path={"/posts/:id"} exact component={FullPost}/>
                     {/* <Route path='/study' component={Study}/>
                     <Route path='/job' component={Job}/> */}
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom';
+import {withRouter, Redirect} from 'react-router-dom';
 import './NewPost.css';
 import axios from 'axios';
 
@@ -7,43 +7,66 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max',
+        author: '',
+        email: 'sdfg4@naver.com',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhd3NTZXJ2ZXIiLCJleHAiOjE2MDExOTU0NTksImlhdCI6MTYwMTE5NDg1OX0.LgdsG-OWmYuYmnOkg8BMesAZTpqZFHjSOjopxPkhAmM',
     }
 
     postHandler = () => {
         const post = {
             title: this.state.title,
-            content: this.state.content,
-            author: this.state.author
+            contents: this.state.content,
+            writer: this.state.author,
+            email: this.state.email,        // this.props.
+            token: this.state.token         // this.props.
         }
-        axios.post('https://jsonplaceholder.typicode.com/posts', post)
+        axios.post('/frontQABoard/create', post)
         .then(response => {
             console.log(response);
-            this.props.history.push('/posts'); 
-        }); 
+            this.props.history.push('/qa'); 
+        })
+        .catch(error => console.log(error)) 
+    }
+
+    toListHandler = () => {
+        this.props.history.push('/qa')
     }
 
     render () {
-        // let redirect = null;
-        // if(this.state.submited) {
-        //     redirect = <Redirect to="/posts"/>
-        // }
+     /* let redirect = null;
+        if(this.state.submited) {
+            redirect = <Redirect to="/posts"/>
+            } */
         return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                <button onClick={this.postHandler}>Add Post</button>
-            </div>
+            <section>
+                <div className="NewPost">
+                    <h1>Add a Post</h1>
+                    <label>Title</label>
+                    <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                    <label>Content</label>
+                    <textarea rows="10" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                    <label>Author</label>
+                    <input type="text" value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}/>
+                    <button onClick={this.postHandler}>Add Post</button>
+                </div>
+                <div className="tolist">
+                    <button onClick={this.toListHandler}>To List</button>
+                </div>
+            </section>
         );
     }
 }
 
-export default withRouter(NewPost); //withRouter로 <Suspense>안에서 이 컴포넌트를 호출해도, 이 컴포넌트 내에서 this.props를 받을 수 있다.
+export default NewPost
+
+//export default withRouter(NewPost); withRouter로 <Suspense>안에서 이 컴포넌트를 호출해도, 이 컴포넌트 내에서 this.props를 받을 수 있다.
+
+/*
+{
+	"writer": "nick",
+	"title": "article3",
+	"contents": "this is a first article gg",
+    "email" : "sdfg4@naver.com",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhd3NTZXJ2ZXIiLCJleHAiOjE2MDExOTU0NTksImlhdCI6MTYwMTE5NDg1OX0.LgdsG-OWmYuYmnOkg8BMesAZTpqZFHjSOjopxPkhAmM"
+}
+*/
